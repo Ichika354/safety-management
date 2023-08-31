e8c5c06664bbbe199fd536d4f49499b87a842bda
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-08-31 17:28:07 +0700

    done bg

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -37,7 +37,7 @@ if (isset($_POST["submit"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Dashboard - SB Admin</title>
+    <title>Reporter</title>
     <link href="../../css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="../../css/dashboar.css">
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
